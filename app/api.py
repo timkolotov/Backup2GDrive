417149e678b20a1e28e4748423a1a92e9b13172b
Tim Kolotov <timophey.kolotov@gmail.com>
2018-07-14 21:50:20 +0300

    Updated: google api moved to separate class

---
+++ app/api.py
@@ -0,0 +1,90 @@
+import time
+
+from apiclient import discovery, http
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+from utils import print_log
+
+
+class ApiClient(object):
+    scopes = 'https://www.googleapis.com/auth/drive'
+    store = file.Storage('./conf.d/credentials.json')
+    service = None
+
+    def __init__(self, path):
+        self.drive_dirs = path.split('/')[1:]
+
+    def setup(self):
+        """ Setup the Drive v3 API """
+
+        creds = self.store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets(
+                './conf.d/client_id.json', self.scopes)
+            creds = tools.run_flow(flow, self.store)
+
+        self.service = discovery.build(
+            'drive', 'v3', http=creds.authorize(Http()))
+        print_log('GDrive API client has been configured')
+
+    def upload_backup(self, filename):
+        """ Upload backup file to GDrive """
+
+        # determine Google Drive directory id for uploading backup file
+        directory = self.get_dir_id()
+
+        print_log('Uploading backup file is started')
+
+        metadata = {'name': filename.split('/').pop(), 'parents': [directory]}
+        file_body = http.MediaFileUpload(
+            filename, 'application/octet-stream', chunksize=1024*1024*2,
+            resumable=True)
+        try:
+            self.service.files().create(
+                body=metadata, media_body=file_body).execute()
+        except http.HttpError:
+            print_log('Uploading backup file is failed')
+            return False
+        else:
+            print_log('Uploading backup file is finished successfully')
+            return True
+
+    def create_dir(self, dir_name, parent_id=None):
+        """ Create new directory """
+
+        metadata = {
+            'name': dir_name,
+            'parents': [parent_id if parent_id else 'root'],
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        result = self.service.files().create(body=metadata).execute()
+        dir_id = result.get('id', [])
+        # start recursive creation if not find parent directory
+        if len(self.drive_dirs) > 0:
+            dir_id = self.create_dir(self.drive_dirs.pop(0), dir_id)
+        return dir_id
+
+    def get_dir_id(self, parent=None):
+        """ Looking for specified path """
+
+        query = "mimeType = 'application/vnd.google-apps.folder' and '{id}' " \
+                "in parents and trashed = false and name = '{name}'"
+
+        # if exception raised, then it was last directory - stop recursion
+        try:
+            current_dir = self.drive_dirs.pop(0)
+        except IndexError:
+            return parent
+
+        result = self.service.files().list(
+            q=query.format(id=parent if parent else 'root', name=current_dir)
+        ).execute()
+        time.sleep(.1)
+
+        directories = result.get('files', [])
+        if directories:
+            dir_id = self.get_dir_id(parent=directories.pop().get('id'))
+        else:
+            dir_id = self.create_dir(current_dir, parent)
+        return dir_id
--- app/backup.py
+++ app/backup.py
@@ -1,86 +1,10 @@
+import json
 import os
 import time
-import json
-import pytz
 from subprocess import call
 
-from apiclient import discovery, http
-from httplib2 import Http
-from oauth2client import file, client, tools
+from api import ApiClient
 from utils import print_log
-
-
-def setup_api():
-    """ Setup the Drive v3 API """
-
-    scopes = 'https://www.googleapis.com/auth/drive'
-    store = file.Storage('./conf.d/credentials.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('./conf.d/client_id.json', scopes)
-        creds = tools.run_flow(flow, store)
-
-    print_log('GDrive API client has been configured')
-    return discovery.build('drive', 'v3', http=creds.authorize(Http()))
-
-
-def create_dir(dir_name, parent_id=None):
-    """ Create new directory """
-
-    metadata = {
-        'name': dir_name,
-        'parents': [parent_id if parent_id else 'root'],
-        'mimeType': 'application/vnd.google-apps.folder'
-    }
-    result = SERVICE.files().create(body=metadata).execute()
-    dir_id = result.get('id', [])
-    # start recursive creation if not find parent directory
-    if len(DRIVE_DIRS) > 0:
-        dir_id = create_dir(DRIVE_DIRS.pop(0), dir_id)
-    return dir_id
-
-
-def get_dir_id(parent=None):
-    """ Looking for specified path """
-
-    query = "mimeType = 'application/vnd.google-apps.folder' and '{id}' " \
-            "in parents and trashed = false and name = '{name}'"
-
-    # if exception raised, then it was last directory - stop recursion
-    try:
-        current_directory = DRIVE_DIRS.pop(0)
-    except IndexError:
-        return parent
-
-    result = SERVICE.files().list(
-        q=query.format(id=parent if parent else 'root', name=current_directory)
-    ).execute()
-    time.sleep(.1)
-
-    directories = result.get('files', [])
-    if directories:
-        dir_id = get_dir_id(parent=directories.pop().get('id'))
-    else:
-        dir_id = create_dir(current_directory, parent)
-    return dir_id
-
-
-def upload_backup(directory, filename):
-    """ Upload backup file to GDrive """
-
-    print_log('Uploading backup file is started')
-
-    metadata = {'name': filename.split('/').pop(), 'parents': [directory]}
-    file_body = http.MediaFileUpload(filename, 'application/octet-stream',
-                                     chunksize=1024*1024*2, resumable=True)
-    try:
-        SERVICE.files().create(body=metadata, media_body=file_body).execute()
-    except http.HttpError:
-        print_log('Uploading backup file is failed')
-        return False
-    else:
-        print_log('Uploading backup file is finished successfully')
-        return True
 
 
 def make_backup(files, exclude, name, passphrase, **kwargs):
@@ -141,9 +65,8 @@ if __name__ == '__main__':
     os.environ['TZ'] = config.pop('timezone', '')
     time.tzset()
 
-    # must be global
-    DRIVE_DIRS = config['drive_path'].split('/')[1:]
-    SERVICE = setup_api()
+    ac = ApiClient(path=config['drive_path'])
+    ac.setup()
 
     if 'run_before' in config:
         print_log('Execution \'run_before\' command is started')
@@ -152,11 +75,8 @@ if __name__ == '__main__':
     # create backup
     path_to_backup_file = make_backup(**config)
 
-    # determine Google Drive directory id for uploading backup file
-    directory_id = get_dir_id()
-
     # upload backup file and remove local file
-    upload_result = upload_backup(directory_id, path_to_backup_file)
+    upload_result = ac.upload_backup(path_to_backup_file)
     if upload_result:
         os.unlink(path_to_backup_file)
 
